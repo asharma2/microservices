2a225ceaa91f4978f48aa22b1bf3eb79205668b9
Atul Sharma <atulkumarsharma1986@gmail.com>
2017-12-25 13:35:25 +0530

    microservices

---
+++ microservices/src/main/java/com/aks/App.java
@@ -0,0 +1,13 @@
+package com.aks;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
